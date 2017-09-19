76839982cb5ebc088b6cdd1f4a865c25c9dfbcba
Pablo Jurado <pjurado85@gmail.com>
2017-09-18 19:31:05 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,40 @@
+// -----------------------------------------------------------------------------
+//    PART 1
+// -----------------------------------------------------------------------------
+
+// Add form / input validation to the registrationform div with Javascript.
+// Please do not use a JavaScript library to do form validation.
+
+
+
+
+
+// -----------------------------------------------------------------------------
+//    PART 2
+// -----------------------------------------------------------------------------
+
+// Create a javascript based pop up container using the YUI library that
+// displays the contents of http://www.cpanel.net when the
+// Find out More button is activated.
+
+
+
+
+// -----------------------------------------------------------------------------
+//    PART 3
+// -----------------------------------------------------------------------------
+
+// Remove every third element from the array listed below using
+// Javascript and without creating a new array.
+
+
+
+
+
+// -----------------------------------------------------------------------------
+//    PART 4
+// -----------------------------------------------------------------------------
+
+// Parse this Flickr JSON feed without using eval() and output a picture and
+// description for the each photo in the feed into a div when
+// the show pictures button is clicked
