58ada27ae881b8fe5e0d49c725584f84254e376f
Pablo Jurado <pjurado85@gmail.com>
2017-09-19 22:15:34 -0500

    create feedback function to avoid repeating the same code

--- js/main.js
+++ js/main.js
@@ -5,35 +5,29 @@
 // Add form / input validation to the registrationform div with Javascript.
 // Please do not use a JavaScript library to do form validation.
 
-// geoglocation: currently no input validation is used as the input validation would be different for US and non-US locations
+function addButtonsEvents () {
+  var submitBtn = document.getElementById('submit_btn');
+  var usernameEl = document.getElementById('username');
+  var passwordEl = document.getElementById('password');
+  var usernumberEl = document.getElementById('usernumber');
 
-var submitBtn = document.getElementById('submit_btn');
-var usernameEl = document.getElementById('username');
-var passwordEl = document.getElementById('password');
-var usernumberEl = document.getElementById('usernumber');
-var geo = document.getElementById('geoglocation');
-var feedback = document.getElementById('feedback');
-
-// main validation event
-submitBtn.onclick = validateForm;
-// username validation event
-usernameEl.onchange = checkUsername;
-// password validation event
-passwordEl.onchange = checkPassword;
-// usernumber validation event
-usernumberEl.onchange = checkUserNumber;
+  // main validation event
+  submitBtn.onclick = validateForm;
+  // username validation event
+  usernameEl.onchange = checkUsername;
+  // password validation event
+  passwordEl.onchange = checkPassword;
+  // usernumber validation event
+  usernumberEl.onchange = checkUserNumber;
+}
 
 function checkUsername (event) {
-  var usernameEl = event.target;
+  var domElement = event.target;
   var username = event.target.value;
+  var feedback = 'User Name must contain a lower and upper case letter and at least 1 number (no special characters)';
 
-  if (isValidUsername(username)) {
-    usernameEl.classList.remove('invalid');
-    feedback.innerHTML = '';
-  } else {
-    usernameEl.classList.add('invalid');
-    feedback.innerHTML = 'User Name must contain a lower and upper case letter and at least 1 number (no special characters)';
-  }
+  if (isValidUsername(username)) removeFeedback(domElement);
+  else renderFeedback(domElement, feedback);
 }
 
 // username must contain a lower and upper case letter and at least 1 number. Cannot contain special characters
@@ -45,16 +39,12 @@ function isValidUsername (name) {
 }
 
 function checkPassword (event) {
-  var passwordEl = event.target;
+  var domElement = event.target;
   var password = event.target.value;
+  var feedback = 'Password must contain at least 2 numbers and be 8 to 15 characters';
 
-  if (isValidPassword(password)) {
-    passwordEl.classList.remove('invalid');
-    feedback.innerHTML = '';
-  } else {
-    passwordEl.classList.add('invalid');
-    feedback.innerHTML = 'Password must contain at least 2 numbers and be 8 to 15 characters';
-  }
+  if (isValidPassword(password)) removeFeedback(domElement);
+  else renderFeedback(domElement, feedback);
 }
 
 // password must contain at least 2 numbers and be 8 to 15 characters in length
@@ -66,16 +56,12 @@ function isValidPassword (pass) {
 }
 
 function checkUserNumber (event) {
-  var usernumberEl = event.target;
+  var domElement = event.target;
   var usernumber = event.target.value;
+  var feedback = 'User number must be 36 digits';
 
-  if (isValidUserNumber(usernumber)) {
-    usernumberEl.classList.remove('invalid');
-    feedback.innerHTML = '';
-  } else {
-    usernumberEl.classList.add('invalid');
-    feedback.innerHTML = 'User number must be 36 digits';
-  }
+  if (isValidUserNumber(usernumber)) removeFeedback(domElement);
+  else renderFeedback(domElement, feedback);
 }
 
 // usernumber must be 36 digits and have no non-numeral characters
@@ -86,27 +72,28 @@ function isValidUserNumber (number) {
   return false;
 }
 
+var geoEl = document.getElementById('geoglocation');
+
 function geoSuccess (pos) {
   var crd = pos.coords;
   var lat = crd.latitude;
   var lng = crd.longitude;
 
-  geo.value = 'Lat:' + lat + ', Lng: ' + lng;
+  geoEl.value = 'Lat:' + lat + ', Lng: ' + lng;
 }
 
 function geoError (err) {
   console.warn(err);
-  geo.innerHTML = 'Geolocation is not supported.';
+  geoEl.value = 'Geolocation is not supported.';
 }
 
 function getPosition () {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(geoSuccess, geoError);
   } else {
-    geo.innerHTML = 'Geolocation is not supported.';
+    geoEl.value = 'Geolocation is not supported.';
   }
 }
-getPosition();
 
 function validateForm (event) {
   event.preventDefault();
@@ -117,6 +104,20 @@ function validateForm (event) {
 
   // TODO: validate
   console.log('validate', username, password, usernumber);
+}
+
+function renderFeedback (element, string) {
+  var feedback = document.getElementById('feedback');
+
+  element.classList.add('invalid');
+  feedback.innerHTML = string;
+}
+
+function removeFeedback (element) {
+  var feedback = document.getElementById('feedback');
+
+  element.classList.remove('invalid');
+  feedback.innerHTML = '';
 }
 
 // RegExp
@@ -147,6 +148,10 @@ function hasSpecialChar (str) {
 function hasOnlyNumbers (str) {
   return /^\d+$/.test(str);
 }
+
+addButtonsEvents();
+getPosition();
+
 
 // -----------------------------------------------------------------------------
 //    PART 2
